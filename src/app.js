70c38f9d8e99b0ee698e67207762c1f0423bc0b0
Basit Ahmad <basitahmad60@gmail.com>
2024-11-08 17:30:14 +0530

    Bmi Calculator P.s autoclear inputarea pending

---
+++ src/app.js
@@ -0,0 +1,38 @@
+function calclulateBMI() {
+    const weight = parseFloat(document.getElementById('weight').value);
+    const height = parseFloat(document.getElementById('height').value);
+    
+
+if (!weight || !height){
+    document.getElementById('result').textContent = "Please enter the weight and height";
+    return;
+}
+ const bmi = weight / height ** 2;
+ let category;
+
+ if (bmi < 18.5)
+    {
+    category = 'Underweight';
+ } 
+ else if(bmi >=18.5 && bmi <= 24.9)
+ {
+    category = 'Normal weight';
+ } 
+ else if(bmi >= 25 && bmi <=29.9)
+ {
+    category = 'Overweight';
+ } 
+ else if(bmi >=30 && bmi< 35)
+ {
+    category = 'Obesity'
+ } 
+ else if(bmi >= 35)
+ {
+    category = 'Severe Obesity'
+ }
+
+ document.getElementById('result').textContent = `Your BMI is ${bmi.toFixed(2)} - ${category}`;
+
+ 
+}
+
